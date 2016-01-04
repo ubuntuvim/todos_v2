a384f2eff35b454280d0bae408ec85d65ddcc055
ubuntuvim <1527254027@qq.com>
2016-01-05 01:27:20 +0800

    增加登录后跳转判断

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    
+    session: Ember.inject.service('session'),
+    model: function() {
+        //  如果已经登录过的直接转到首页
+        if (this.get('session').get('data').authenticated.uid) {
+            // window.location.href = config.localeBaseUrl;
+            this.transitionTo('index');
+        } else {
+            this.transitionTo('login');
+        }
+    }
 });
