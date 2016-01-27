b602a691550ee6f0599a7dd9718490008da8ca71
ubuntuvim <1527254027@qq.com>
2016-01-27 18:29:49 +0800

    加一层过滤：不显示子todo数量

--- app/components/no-completed-todos-count-by-classify.js
+++ app/components/no-completed-todos-count-by-classify.js
@@ -15,14 +15,17 @@ export default Ember.Component.extend({
     // 三重过滤：1，状态为1；2，登录用户id；3，所属分类
     noCompletedTodoCount4Cmp: Ember.computed('todosForTotla.@each.userId',
                                         'todosForTotla.@each.recordStatus',
-                                        'todosForTotla.@each.project', function() {
+                                        'todosForTotla.@each.project',
+                                        'todosForTotla.@each.isChildOrParent', function() {
 		var userId = this.get('session').get('data').authenticated.uid;
         var projCode = this.get('projCode');  //调用组件时候传递过来
 
         return this.get('todosForTotla').filter(function(td) {
             return td.get('recordStatus') === 1
                     && td.get('user') === userId
-                    && td.get('project') === projCode;
+                    && td.get('project') === projCode
+                    && td.get('isChildOrParent') != 1
+                    && td.get('isChildOrParent') != 2;
         }).get('length');
 	})
 });
