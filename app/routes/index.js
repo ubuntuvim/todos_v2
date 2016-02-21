214f66ed079b6f01549781e0403905a412b2c8a6
ubuntuvim <1527254027@qq.com>
2016-02-22 02:09:40 +0800

    修改route:index为欢迎页面route并返回判断用户是否登录的标记

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,31 +2,20 @@
 
 import Ember from 'ember';
 
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import LocalStorageStore from 'ember-simple-auth/session-stores/local-storage';
 
 // 实现AuthenticatedRouteMixin的类会自动根据权限过滤，如果经过登录页面直接进入这个route会自动跳转到登录页面
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(/*AuthenticatedRouteMixin, */{
+
     session: Ember.inject.service('session'),
-
-	// this.transitionTo('index', { queryParams: { recordStatus: 1 } });
-	// queryParams: {
-	// 	recordStatus: {
-	// 		replace: true,
-	// 		refreshModel: true
-	// 	}
-	// },
-
-	//  获取firebase数据并返回到页面
-	// model: function() {
-	// 	return this.store.peekAll('todo-item');
-	// 	// return this.store.findRecord('todoItem', 1);
-	// }
-
-	// afterModel: function() {
-	// 	var data = sessionStorage.getItem('LOGIN_USER_EMAIL');
-	// 	console.log("afterModel  >> " + data);
-	// // 	return { LOGIN_USER_EMAIL: data };
-	// }
+    model: function() {
+        //  页面需要使用登录状态判断显示不用的html
+        if (this.get('session').get('data').authenticated.uid) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 });
