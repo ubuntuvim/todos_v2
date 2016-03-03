dfdc9926d677ca0e181cb37c30face2aaeec48a2
ubuntuvim <1527254027@qq.com>
2016-03-04 01:33:58 +0800

    增加退出登录方法

--- app/components/header-page.js
+++ app/components/header-page.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    session: Ember.inject.service('session'),
+    actions: {
+		/**
+		 * 执行这个方法销毁登录时候保存到session的数据，并且会自动跳转到config/environment.js里配置的登录页面
+		 * @return {[type]} [description]
+		 */
+	    invalidateSession: function() {
+			localStorage.clear();  //销毁登录时候保存到session的数据
+	        this.get('session').invalidate();
+	    }
+	}
 });
